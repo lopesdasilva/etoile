e1f89ed1adaa3a9254199393c22feee01079365f
lopesdasilva <lopesdasilva@10-93-e9-0d-17-48.connectify>
2012-10-24 13:23:09 +0100

    Visual Changes

--- etoile-play/app/controllers/Application.java
+++ etoile-play/app/controllers/Application.java
@@ -66,7 +66,7 @@ public class Application extends Controller {
         session().clear();
         flash("success", "You've been logged out");
         return redirect(
-            routes.Application.login()
+            routes.Application.index()
         );
     }
 	

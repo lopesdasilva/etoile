d6ebc4f55995f8bf80323b5e2ac21f4b45ca5a04
lopesdasilva <lopesdasilva@lopesdasilva-pc.lan>
2012-12-03 11:39:48 +0000

    [Question] Já é possivel introfuzir formulas nas questions

--- etoile-play/app/Global.java
+++ etoile-play/app/Global.java
@@ -43,7 +43,7 @@ public class Global extends GlobalSettings {
 			//INHERITANCE TESTS
 			
 			Question q = new Question();
-			q.question = "This is the First Open Question.";
+			q.question = " What is the name of this formula:  `x = (-b +- sqrt(b^2-4ac))/(2a)` ?";
 			q.imageURL= "http://www.psdgraphics.com/wp-content/uploads/2009/04/growth-chart.jpg";
 			q.videoURL= "http://www.youtube.com/v/AyPzM5WK8ys";
 			q.typeOfQuestion = 0;

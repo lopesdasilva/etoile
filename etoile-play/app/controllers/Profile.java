c14552e4740ca46682e8eb9fccad72c0b6343156
Ruben Paixao <ruben.npaixao@gmail.com>
2012-12-08 04:17:53 +0000

    [Questions] Já guarda as questões(dos três tipos)

--- etoile-play/app/controllers/Profile.java
+++ etoile-play/app/controllers/Profile.java
@@ -256,7 +256,7 @@ public class Profile extends Controller {
 				
 			List<Hypothesis> hypothesis_aux=Hypothesis.findByUserEmailAndQuestion(user.email, q.id);
 			if (hypothesis_aux.size()<1){
-				for (Hypothesis h: Hypothesis.findByQuestion(q.id)){
+				for (Hypothesis h: q.hypothesislist){
 					Hypothesis new_h=new Hypothesis();
 					new_h.number=h.number;
 					new_h.question=h.question;
@@ -470,42 +470,137 @@ public class Profile extends Controller {
 //				form(OneChoiceQuestionAnswer.class)));
 //	}
 
-	public static Result postmultiplechoicequestionanswer(
-			String module_acronym, String lesson_acronym, Long test_id,
+	public static Result postquestion(
+			int question_number, String module_acronym, String lesson_acronym, Long test_id,
 			Long question_id) {
-		Form<Profile.MultipleChoiceQuestionAnswer> form = form(
-				Profile.MultipleChoiceQuestionAnswer.class).bindFromRequest();
-
-		Test test = Test.find.byId(test_id);
-
-		//
-		// if(userMultipleChoiceAnswer.hypothesislist.size()!=0){
-		// for(MultipleChoiceHypothesis hyp:
-		// userMultipleChoiceAnswer.hypothesislist){
-		// userMultipleChoiceAnswer.hypothesislist.remove(hyp);
-		// hyp.save();
-		// userMultipleChoiceAnswer.save();
-		// }
-		// }
-
-		for (int h : form.get().mcqanswers) {
-			System.out.println("VALOR: " + h);
-		}
-
+		
 		User user = User.find.byId(request().username());
-		List<Answer> openanswers = Answer.findByUserEmailAndTestId(user.email,
-				test_id);
+		Question question = Question.find.byId(question_id);
+		
+			if(question.typeOfQuestion == 1){
+				Form<Profile.OneChoiceQuestionAnswer> form = form(Profile.OneChoiceQuestionAnswer.class).bindFromRequest();
+				List<Hypothesis> last_answers = Hypothesis.findByUserEmailAndQuestion(user.email, question_id); // Respostas Guardadas
+				for(Hypothesis h : last_answers){
+					h.selected = false;
+					h.save();
+				}
+				
+				Hypothesis hypothesis = Hypothesis.find.byId(form.get().ocqanswer);
+				hypothesis.selected = true;
+				hypothesis.save();
+				
+			}else if(question.typeOfQuestion == 2) {
+			// GUARDAR ESCOLHA MULTIPLA E ONE CHOICE
 
-		List<Category> categories = Category.getAllCategories();
-		Test test_aux = test;
-		test_aux.answers = openanswers;
-
+			Form<Profile.MultipleChoiceQuestionAnswer> form = form(Profile.MultipleChoiceQuestionAnswer.class).bindFromRequest();
+			for (int h : form.get().mcqanswers) {
+				System.out.println("VALOR: " + h);
+			}
+			
+			List<Hypothesis> last_answers = Hypothesis.findByUserEmailAndQuestion(user.email, question_id); // Respostas Guardadas
+			
+			for(Hypothesis h : last_answers){
+				h.selected = false;
+				h.save();
+				
+			}
+			
+			for(int h : form.get().mcqanswers){
+				if(h!=0){
+					for(Hypothesis hypothesis: last_answers){
+						if(hypothesis.id == h){
+							hypothesis.selected = true;
+							hypothesis.save();
+						}
+					}
+				}
+			}
+		} else {
+			//GUARDAR OPEN QUESTION - WORKING
+			
+			Form<Profile.QuestionAnswer> form = form(Profile.QuestionAnswer.class).bindFromRequest();
+			System.out.println("Open Answer: " + form.get().qanswer);
+			
+			Answer answer = Answer.findByUserAndQuestion(user.email, question_id); // Resposta Guardada
+			answer.answer = form.get().qanswer;
+			answer.save();
+		}
+		
+		//COPIA DO METODO question() - Temos que arranjar maneira de isto não ficar tudo repetido
+		
+		Test test = models.test.Test.find.byId(test_id);
 		Module module = Module.findByAcronym(module_acronym);
 		Lesson lesson = Lesson.findByAcronym(lesson_acronym);
-
-		return ok(views.html.secured.test.render(user, module, lesson,
-				categories, test_aux, form(QuestionAnswer.class),
-				form(OneChoiceQuestionAnswer.class)));
+		UserTest usertest = UserTest.findByUserAndTest(user.email,
+				test.id);
+		
+		if (question_number<=test.groups.size() && question_number>0){
+		System.out.println("A imprimir as questoes");
+		
+		QuestionGroup group = test.groups.get(question_number-1);
+		
+		QuestionGroup group_aux = new QuestionGroup();
+		group_aux=new QuestionGroup();
+		group_aux.id = group.id;
+		group_aux.imageURL = group.imageURL;
+		group_aux.number = group.number;
+		group_aux.question = group.question;
+		group_aux.test = group.test;
+		group_aux.videoURL = group.videoURL;
+		
+		System.out.println("Group_aux created");
+		
+		for(Question q: group.questions){
+			Question q_aux = new Question();
+			q_aux.id = q.id;
+			q_aux.imageURL = q.imageURL;
+			q_aux.lesson = q.lesson;
+			q_aux.number = q.number;
+			q_aux.question = q.question;
+			q_aux.typeOfQuestion = q.typeOfQuestion;
+			q_aux.user = q.user;
+			q_aux.videoURL = q.videoURL;
+			q_aux.urls=q.urls;
+			
+			
+			//q_aux.hypothesislist?????
+			group_aux.questions.add(q_aux);
+			
+			System.out.println("QUESTION: "+q.id);
+			if(q.typeOfQuestion==2 || q.typeOfQuestion == 1){
+				
+			List<Hypothesis> hypothesis_aux=Hypothesis.findByUserEmailAndQuestion(user.email, q.id);
+			if (hypothesis_aux.size()<1){
+				for (Hypothesis h: q.hypothesislist){
+					Hypothesis new_h=new Hypothesis();
+					new_h.number=h.number;
+					new_h.question=h.question;
+					new_h.text=h.text;
+					new_h.user=user;
+					new_h.save();
+				}
+				hypothesis_aux=Hypothesis.findByUserEmailAndQuestion(user.email, q.id);
+			}
+			
+			q_aux.hypothesislist=hypothesis_aux;
+			q=q_aux;
+			}
+			else{
+				q_aux.openanswer=Answer.findByUserAndQuestion( user.email,q.id);
+				q=q_aux;
+			}
+			
+			
+			
+			}
+		
+		
+		return ok(views.html.secured.question.question.render(user,module,lesson,test,group_aux,usertest));
+		}
+		else
+			return ok(views.html.statics.error.render());
+		
+		//FIM COPIA DO METODO question()
 	}
 
 	// -- Queries
